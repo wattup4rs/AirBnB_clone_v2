1279ec94ffa2eed9613a2bdd5b488f55ef07891b
wattup4rs <gideonwatt@gmail.com>
2024-03-26 03:12:53 -0700

    succesful_deploy_sudo fab -f 3-deploy_web_static.py deploy -i ~/.ssh/school -u ubuntu

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -7,7 +7,7 @@ distributes an archive to the web servers
 from fabric.api import env, local, put, run
 from datetime import datetime
 from os.path import exists, isdir
-env.hosts = ['142.44.167.228', '144.217.246.195']
+env.hosts = ['34.239.255.104', '54.85.204.171']
 
 
 def do_pack():
